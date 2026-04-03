53451d4349a2446070b787a23b5e8c9f3e6172d8
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-04-03 15:45:35 -0300

    Adicionado o Lenis
    Adicionado o lenis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,125 @@
+import { useEffect, useRef, useState, useCallback } from 'react';
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
 import Layout from './components/Layout/Layout';
 import Banner from './components/Banner/Banner';
 import About from './components/About/About';
-
 function App() {
+  const lenisRef = useRef(null);
+  const [showScrollTop, setShowScrollTop] = useState(false);
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenisRef.current = lenis;
+    let rafId;
+    function raf(time) {
+      lenis.raf(time);
+      rafId = requestAnimationFrame(raf);
+    }
+    rafId = requestAnimationFrame(raf);
+    const onScroll = () => {
+      setShowScrollTop(lenis.scroll > 200);
+    };
+    lenis.on('scroll', onScroll);
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+      lenisRef.current = null;
+    };
+  }, []);
+  const scrollToTop = useCallback(() => {
+    lenisRef.current?.scrollTo(0, { duration: 1.2 });
+  }, []);
   return (
     <Layout>
       <div className="App">
-        <Banner></Banner>
-        <About></About>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat mi ac vestibulum tincidunt. Donec volutpat tellus a nisl viverra dictum. Maecenas nisl ligula, euismod at interdum id, imperdiet sit amet neque. Donec nec enim erat. Integer eleifend leo et rutrum blandit. Vestibulum tincidunt dui at luctus consectetur. Mauris ullamcorper dapibus congue. Duis tempus elit sed nibh porta, sed mollis dolor rutrum. Quisque gravida at odio quis posuere. Cras aliquam risus mauris, sed sollicitudin quam scelerisque sed. Ut vel ultrices lacus, sit amet congue velit. Integer egestas libero sed laoreet porta. Maecenas sit amet sapien mattis, aliquam augue a, sollicitudin turpis. Vestibulum fermentum tempor eros. Vivamus id tincidunt velit.
-          In ac odio consectetur, finibus diam eu, lobortis ante. Duis varius iaculis enim vestibulum blandit. Ut lacinia neque in rutrum tempor. Nulla volutpat sit amet orci ut ultrices. Sed vehicula urna mi, vitae facilisis arcu sagittis ac. Quisque convallis dictum sagittis. Sed tempor accumsan lectus. Integer nec viverra quam. Morbi varius ornare dignissim.
-          Duis lorem enim, maximus sit amet neque a, pulvinar viverra mauris. Phasellus in nulla eu urna egestas dignissim. Integer metus tellus, ullamcorper id orci sit amet, tempor faucibus massa. Nulla mollis ante nec pharetra tincidunt. Nulla fringilla laoreet suscipit. Pellentesque mattis dolor ut neque eleifend, in mattis dolor tempor. Mauris viverra, leo ut auctor tempor, nisl tortor commodo lorem, non egestas tellus ipsum in mauris. Nam sed sem nec orci eleifend malesuada ultrices sed risus. Suspendisse sit amet erat egestas, rutrum odio id, consequat nisl. Etiam placerat varius massa, non semper massa tempus volutpat. Nullam rutrum in nulla ac luctus. Donec sodales ornare turpis at porta.
-          Phasellus enim velit, porta quis diam ac, egestas placerat nibh. Mauris aliquet et eros et vehicula. Donec consectetur diam nisl. Suspendisse eget sapien vitae lacus blandit facilisis. Sed non nulla nec velit luctus porta. Quisque ut erat tempus, placerat sem ornare, sagittis enim. Ut pellentesque viverra tristique. Vestibulum commodo, libero non egestas aliquet, eros elit mattis nisl, sed semper massa risus non eros. Curabitur eu consectetur elit. Aliquam hendrerit leo sed nisl consectetur, vitae varius purus accumsan.
-          Donec a mi ullamcorper, molestie tellus a, pharetra dolor. Ut ut ultricies sem. Nunc vehicula quam elementum finibus sollicitudin. Donec a suscipit massa. Donec vitae vehicula magna. Ut lacinia lorem non arcu gravida, eget scelerisque turpis lacinia. In ultricies metus ac iaculis pharetra.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat mi ac vestibulum tincidunt. Donec volutpat tellus a nisl viverra dictum. Maecenas nisl ligula, euismod at interdum id, imperdiet sit amet neque. Donec nec enim erat. Integer eleifend leo et rutrum blandit. Vestibulum tincidunt dui at luctus consectetur. Mauris ullamcorper dapibus congue. Duis tempus elit sed nibh porta, sed mollis dolor rutrum. Quisque gravida at odio quis posuere. Cras aliquam risus mauris, sed sollicitudin quam scelerisque sed. Ut vel ultrices lacus, sit amet congue velit. Integer egestas libero sed laoreet porta. Maecenas sit amet sapien mattis, aliquam augue a, sollicitudin turpis. Vestibulum fermentum tempor eros. Vivamus id tincidunt velit.
-          In ac odio consectetur, finibus diam eu, lobortis ante. Duis varius iaculis enim vestibulum blandit. Ut lacinia neque in rutrum tempor. Nulla volutpat sit amet orci ut ultrices. Sed vehicula urna mi, vitae facilisis arcu sagittis ac. Quisque convallis dictum sagittis. Sed tempor accumsan lectus. Integer nec viverra quam. Morbi varius ornare dignissim.
-          Duis lorem enim, maximus sit amet neque a, pulvinar viverra mauris. Phasellus in nulla eu urna egestas dignissim. Integer metus tellus, ullamcorper id orci sit amet, tempor faucibus massa. Nulla mollis ante nec pharetra tincidunt. Nulla fringilla laoreet suscipit. Pellentesque mattis dolor ut neque eleifend, in mattis dolor tempor. Mauris viverra, leo ut auctor tempor, nisl tortor commodo lorem, non egestas tellus ipsum in mauris. Nam sed sem nec orci eleifend malesuada ultrices sed risus. Suspendisse sit amet erat egestas, rutrum odio id, consequat nisl. Etiam placerat varius massa, non semper massa tempus volutpat. Nullam rutrum in nulla ac luctus. Donec sodales ornare turpis at porta.
-          Phasellus enim velit, porta quis diam ac, egestas placerat nibh. Mauris aliquet et eros et vehicula. Donec consectetur diam nisl. Suspendisse eget sapien vitae lacus blandit facilisis. Sed non nulla nec velit luctus porta. Quisque ut erat tempus, placerat sem ornare, sagittis enim. Ut pellentesque viverra tristique. Vestibulum commodo, libero non egestas aliquet, eros elit mattis nisl, sed semper massa risus non eros. Curabitur eu consectetur elit. Aliquam hendrerit leo sed nisl consectetur, vitae varius purus accumsan.
-          Donec a mi ullamcorper, molestie tellus a, pharetra dolor. Ut ut ultricies sem. Nunc vehicula quam elementum finibus sollicitudin. Donec a suscipit massa. Donec vitae vehicula magna. Ut lacinia lorem non arcu gravida, eget scelerisque turpis lacinia. In ultricies metus ac iaculis pharetra.
+        <Banner />
+        <About />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat mi ac
+          vestibulum tincidunt. Donec volutpat tellus a nisl viverra dictum. Maecenas nisl ligula,
+          euismod at interdum id, imperdiet sit amet neque. Donec nec enim erat. Integer eleifend leo
+          et rutrum blandit. Vestibulum tincidunt dui at luctus consectetur. Mauris ullamcorper
+          dapibus congue. Duis tempus elit sed nibh porta, sed mollis dolor rutrum. Quisque gravida
+          at odio quis posuere. Cras aliquam risus mauris, sed sollicitudin quam scelerisque sed. Ut
+          vel ultrices lacus, sit amet congue velit. Integer egestas libero sed laoreet porta.
+          Maecenas sit amet sapien mattis, aliquam augue a, sollicitudin turpis. Vestibulum fermentum
+          tempor eros. Vivamus id tincidunt velit.
+          In ac odio consectetur, finibus diam eu, lobortis ante. Duis varius iaculis enim vestibulum
+          blandit. Ut lacinia neque in rutrum tempor. Nulla volutpat sit amet orci ut ultrices. Sed
+          vehicula urna mi, vitae facilisis arcu sagittis ac. Quisque convallis dictum sagittis. Sed
+          tempor accumsan lectus. Integer nec viverra quam. Morbi varius ornare dignissim. Duis lorem
+          enim, maximus sit amet neque a, pulvinar viverra mauris. Phasellus in nulla eu urna egestas
+          dignissim. Integer metus tellus, ullamcorper id orci sit amet, tempor faucibus massa. Nulla
+          mollis ante nec pharetra tincidunt. Nulla fringilla laoreet suscipit. Pellentesque mattis
+          dolor ut neque eleifend, in mattis dolor tempor. Mauris viverra, leo ut auctor tempor, nisl
+          tortor commodo lorem, non egestas tellus ipsum in mauris. Nam sed sem nec orci eleifend
+          malesuada ultrices sed risus. Suspendisse sit amet erat egestas, rutrum odio id, consequat
+          nisl. Etiam placerat varius massa, non semper massa tempus volutpat. Nullam rutrum in nulla
+          ac luctus. Donec sodales ornare turpis at porta. Phasellus enim velit, porta quis diam ac,
+          egestas placerat nibh. Mauris aliquet et eros et vehicula. Donec consectetur diam nisl.
+          Suspendisse eget sapien vitae lacus blandit facilisis. Sed non nulla nec velit luctus porta.
+          Quisque ut erat tempus, placerat sem ornare, sagittis enim. Ut pellentesque viverra
+          tristique. Vestibulum commodo, libero non egestas aliquet, eros elit mattis nisl, sed semper
+          massa risus non eros. Curabitur eu consectetur elit. Aliquam hendrerit leo sed nisl
+          consectetur, vitae varius purus accumsan. Donec a mi ullamcorper, molestie tellus a,
+          pharetra dolor. Ut ut ultricies sem. Nunc vehicula quam elementum finibus sollicitudin.
+          Donec a suscipit massa. Donec vitae vehicula magna. Ut lacinia lorem non arcu gravida, eget
+          scelerisque turpis lacinia. In ultricies metus ac iaculis pharetra. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Suspendisse placerat mi ac vestibulum tincidunt. Donec
+          volutpat tellus a nisl viverra dictum. Maecenas nisl ligula, euismod at interdum id,
+          imperdiet sit amet neque. Donec nec enim erat. Integer eleifend leo et rutrum blandit.
+          Vestibulum tincidunt dui at luctus consectetur. Mauris ullamcorper dapibus congue. Duis
+          tempus elit sed nibh porta, sed mollis dolor rutrum. Quisque gravida at odio quis posuere.
+          Cras aliquam risus mauris, sed sollicitudin quam scelerisque sed. Ut vel ultrices lacus, sit
+          amet congue velit. Integer egestas libero sed laoreet porta. Maecenas sit amet sapien
+          mattis, aliquam augue a, sollicitudin turpis. Vestibulum fermentum tempor eros. Vivamus id
+          tincidunt velit. In ac odio consectetur, finibus diam eu, lobortis ante. Duis varius iaculis
+          enim vestibulum blandit. Ut lacinia neque in rutrum tempor. Nulla volutpat sit amet orci ut
+          ultrices. Sed vehicula urna mi, vitae facilisis arcu sagittis ac. Quisque convallis dictum
+          sagittis. Sed tempor accumsan lectus. Integer nec viverra quam. Morbi varius ornare
+          dignissim. Duis lorem enim, maximus sit amet neque a, pulvinar viverra mauris. Phasellus in
+          nulla eu urna egestas dignissim. Integer metus tellus, ullamcorper id orci sit amet, tempor
+          faucibus massa. Nulla mollis ante nec pharetra tincidunt. Nulla fringilla laoreet suscipit.
+          Pellentesque mattis dolor ut neque eleifend, in mattis dolor tempor. Mauris viverra, leo ut
+          auctor tempor, nisl tortor commodo lorem, non egestas tellus ipsum in mauris. Nam sed sem nec
+          orci eleifend malesuada ultrices sed risus. Suspendisse sit amet erat egestas, rutrum odio
+          id, consequat nisl. Etiam placerat varius massa, non semper massa tempus volutpat. Nullam
+          rutrum in nulla ac luctus. Donec sodales ornare turpis at porta. Phasellus enim velit, porta
+          quis diam ac, egestas placerat nibh. Mauris aliquet et eros et vehicula. Donec consectetur
+          diam nisl. Suspendisse eget sapien vitae lacus blandit facilisis. Sed non nulla nec velit
+          luctus porta. Quisque ut erat tempus, placerat sem ornare, sagittis enim. Ut pellentesque
+          viverra tristique. Vestibulum commodo, libero non egestas aliquet, eros elit mattis nisl,
+          sed semper massa risus non eros. Curabitur eu consectetur elit. Aliquam hendrerit leo sed
+          nisl consectetur, vitae varius purus accumsan. Donec a mi ullamcorper, molestie tellus a,
+          pharetra dolor. Ut ut ultricies sem. Nunc vehicula quam elementum finibus sollicitudin.
+          Donec a suscipit massa. Donec vitae vehicula magna. Ut lacinia lorem non arcu gravida, eget
+          scelerisque turpis lacinia. In ultricies metus ac iaculis pharetra.
         </p>
-      </div >
+        {showScrollTop && (
+          <button
+            type="button"
+            onClick={scrollToTop}
+            aria-label="Voltar ao topo"
+            style={{
+              position: 'fixed',
+              right: '2.99rem',
+              bottom: '2.99rem',
+              zIndex: 9999,
+              padding: '1rem 1.5rem',
+              cursor: 'pointer',
+              borderRadius: '360px',
+              border: '1px solid var(--bordo, #530505)',
+              background: 'var(--bordo, #530505)',
+              color: 'var(--branco-puro, #fff)',
+              fontSize: '0.9rem',
+              fontSize: '20px',
+            }}
+          >
+            ↑
+          </button>
+        )}
+      </div>
     </Layout>
   );
 }
-
 export default App;
